c47eda86c3f8bbffb9ab2fb2aa7e17cb21ecf21a
JC12211 <31252398+JC12211@users.noreply.github.com>
2020-04-04 15:13:34 -0400

    added reactions that SHOULD work, dear God please

--- bot.js
+++ bot.js
@@ -1,17 +1,74 @@
 var HTTPS = require('https');
-var cool = ['¯\\_(ツ)_/¯', '(ﾟ∩ﾟ)', '(ノಠ益ಠ)ノ彡┻━┻', '(▀̿Ĺ̯▀̿ ̿)'];
+var cool = ['¯\\_(ツ)_/¯', '(ﾟ∩ﾟ)', '(ノಠ益ಠ)ノ彡┻━┻', '(▀̿Ĺ̯▀̿ ̿)', '( ͡° ͜ʖ ͡°)', '( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)', 'ヽ( •_)ᕗ',
+               '(☞ﾟ∀ﾟ)☞',  '(ง •̀_•́)ง', '(｡)(｡)'];
 
 var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      shrug = /^\/cool guy$/;
+      shrug = /^\/shrug$/;
+      sadface = /^\/sadface$/;
+      tableFlip = /^\/table flip$/;
+      tooCool = /^\/too cool$/;
+      happy = /^\/happy$/;
+      gang = /^\/gang$/;
+      dab = /^\/dab$/;
+      myMan = /^\/my$/;
+      fight = /^\/fight me$/;
+      boobies = /^\/boobies$/;
 
   if(request.text && shrug.test(request.text)) {
     this.res.writeHead(200);
     postMessage(0);
     this.res.end();
-  } else {
+  }
+  else if(request.text && sadface.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(1);
+    this.res.end()
+  }
+  else if(request.text && tableFlip.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(2);
+    this.res.end()
+  }
+  else if(request.text && tooCool.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(3);
+    this.res.end()
+  }
+  else if(request.text && happy.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(4);
+    this.res.end()
+  }
+  else if(request.text && gang.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(5);
+    this.res.end()
+  }
+  else if(request.text && dab.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(6);
+    this.res.end()
+  }
+  else if(request.text && myMan.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(7);
+    this.res.end()
+  }
+  else if(request.text && fight.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(8);
+    this.res.end()
+  }
+  else if(request.text && boobies.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage(9);
+    this.res.end()
+  }
+  
+  else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
